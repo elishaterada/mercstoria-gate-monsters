ad9d22489d471edc1920987d208158a0e46af5ff
Elisha Terada <elishaterada@gmail.com>
2016-09-11 18:06:18 -0700

    Basic Angular and Angular Material integration setup

---
+++ src/app/app.js
@@ -0,0 +1,43 @@
+function config (
+  $mdThemingProvider
+) {
+
+  // Angular Material Setup
+  $mdThemingProvider.theme('custom')
+    .primaryPalette('deep-purple')
+    .accentPalette('blue')
+
+  $mdThemingProvider.setDefaultTheme('custom')
+
+}
+
+function run ($rootScope, $window) {
+  $rootScope._ = $window._
+}
+
+function AppCtrl () {
+}
+
+angular
+  .module('app', [
+    'ngAnimate',
+    'ngMaterial',
+    'ngAria'
+  ])
+  .controller('AppCtrl', AppCtrl)
+  .config(config)
+  .run(run)
+
+//  Sample Component
+function SampleCtrl () {
+  var ctrl = this
+
+  ctrl.greeting = 'Hello World'
+}
+
+angular
+  .module('app')
+  .component('sample', {
+    templateUrl: 'src/templates/sample.tpl.html',
+    controller: SampleCtrl
+  })
