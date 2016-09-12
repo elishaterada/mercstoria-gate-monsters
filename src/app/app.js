cd6d9812ac40603cf4dad8be51677107d6bcf8d4
Elisha Terada <elishaterada@gmail.com>
2016-09-11 19:23:15 -0700

    Base for monster selections

--- src/app/app.js
+++ src/app/app.js
@@ -26,16 +26,90 @@ angular
   .config(config)
   .run(run)
 
-//  Sample Component
-function SampleCtrl () {
+//  Monsters Component
+function MonstersCtrl ($mdDialog) {
   var ctrl = this
 
-  ctrl.greeting = 'Hello World'
+  ctrl.type = ''
+
+  ctrl.monsters = {
+    fire: [
+
+    ],
+    water: [
+      {
+        name: 'メリギール',
+        reach: 90,
+        range: 140,
+        combo: 3,
+        chosen: false
+      },
+      {
+        name: 'ヴィンデール',
+        reach: 115,
+        range: 150,
+        combo: 0,
+        chosen: false
+      },
+      {
+        name: 'セネガルガ',
+        reach: 70,
+        range: 140,
+        combo: 4,
+        chosen: false
+      },
+      {
+        name: 'アンキロクロス',
+        reach: 40,
+        range: 80,
+        combo: 0,
+        chosen: false
+      }
+
+    ],
+    wind: [
+
+    ],
+    light: [
+
+    ],
+    dark: [
+
+    ]
+  }
+
+  ctrl.chosenMonsters = []
+
+  ctrl.generateMonsterList = function(ev) {
+    $mdDialog.show({
+      controller: MonsterGeneratorCtrl,
+      templateUrl: 'src/templates/monster-list-generator-dialog.tpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: true,
+      locals: {
+        monsters: ctrl.monsters[ctrl.type]
+      }
+    })
+    .then(function(answer) {
+    }, function() {
+    })
+  }
 }
 
 angular
   .module('app')
-  .component('sample', {
-    templateUrl: 'src/templates/sample.tpl.html',
-    controller: SampleCtrl
+  .component('monsters', {
+    templateUrl: 'src/templates/monsters.tpl.html',
+    controller: MonstersCtrl
   })
+
+// Monster Generator Dialog Controller
+function MonsterGeneratorCtrl ($scope, $mdDialog, monsters) {
+
+  $scope.monsters = monsters
+
+  $scope.close = function() {
+    $mdDialog.cancel()
+  }
+}
